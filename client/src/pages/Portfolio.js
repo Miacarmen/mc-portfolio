3d2d28add88f3d9d765cb7be141597fb5285cb7f
Miacarmen <maitreyacarmen@gmail.com>
2022-06-29 13:23:12 -0700

    apollo server initialized

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -69,6 +69,13 @@ const Portfolio = () => {
           <img alt='Mise en Place' src={p3}></img>
         </Card>
       </div>
+
+      <div class='btn-group'>
+        <button class='btn'>1</button>
+        <button class='btn btn-active'>2</button>
+        <button class='btn'>3</button>
+        <button class='btn'>4</button>
+      </div>
     </div>
   );
 };
