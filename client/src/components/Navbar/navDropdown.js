5d009206c9f1952b42e86ecfeebb5561b9a83a98
Miacarmen <maitreyacarmen@gmail.com>
2022-07-05 16:45:55 -0700

    created component for nav dropdown menu

---
+++ client/src/components/Navbar/navDropdown.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+const NavDropdown = () => {
+    const [expandMenu, setExpandMenu] = useState(false);
+  return (
+    <div className='dropdown dropdown-left'>
+          <label
+            tabindex='0'
+            className='dropMenu btn btn-ghost btn-circle'
+            onClick={() => setExpandMenu(!expandMenu)}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              class='h-5 w-5'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                stroke-width='2'
+                d='M4 6h16M4 12h16M4 18h7'
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex='0'
+            className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+          >
+            <li>
+              <Link to='/' className='hover:text-primary-focus'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/portfolio' className='hover:text-primary-focus'>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to='/resume' className='hover:text-primary-focus'>
+                Resume
+              </Link>
+            </li>
+            <li>
+              <Link to='/shop' className='hover:text-primary-focus'>
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link to='about' className='hover:text-primary-focus'>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to='/contact' className='hover:text-primary-focus'>
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to='/cart' className='hover:text-primary-focus'>
+                Cart
+              </Link>
+            </li>
+          </ul>
+        </div>
+  )
+}
+
+export default NavDropdown;
