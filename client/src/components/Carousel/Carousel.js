854d96690e0bcba50dd9d94063288c8b8b8d9483
Miacarmen <maitreyacarmen@gmail.com>
2022-09-06 19:24:38 -0700

    deleted design image files

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -7,7 +7,6 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import cbData from '../../cbData.json';
 
 const ImgCarousel = () => {
-
   return (
     <>
       <Card className='card-title lg:card-side bg-base-100 shadow-xl mb-5'>
@@ -15,20 +14,18 @@ const ImgCarousel = () => {
           <Card.Title tag='h2'>Chubby Snacks</Card.Title>
           {/* <p>{image.title}</p> */}
           <Carousel showArrows autoPlay stopOnHover>
-            {cbData.map((data) => { 
-            return (
-            <div key={data.id}>
-              <p>{data.title}</p>
-              <img alt={`${data.title}`} src={data.imageURL} />
-            </div>
-            );
+            {cbData.map((data) => {
+              return (
+                <div key={data.id}>
+                  <p>{data.title}</p>
+                  <img alt={`${data.title}`} src={data.imageURL} />
+                </div>
+              );
             })}
           </Carousel>
         </Card.Body>
       </Card>
-
     </>
-
   );
 };
 
